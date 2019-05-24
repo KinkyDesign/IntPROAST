7763e206c5598288b824e2a2c61da02fe3710c3a
GTsiliki <g_tsiliki@hotmail.com>
2019-05-24 15:45:18 +0300

    closeConnections
    check again

--- R/enm.proast61.R
+++ R/enm.proast61.R
@@ -72,7 +72,7 @@ function(dataset,predictionFeature,parameters){
     #sink.number() 
     #closeAllConnections()
     graphics.off()
-    
+    #apply(as.matrix(x),1,dev.off)
 
     
     #read print out
